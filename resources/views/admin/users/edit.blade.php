42c59dcf8d6b79bdf0b49c0fe035154b82d3f532
ariacode <ariacode.webdesign@gmail.com>
2019-09-27 20:24:05 +0330

    first save create views

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>website</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="icon" href="assets/iamges/logo.png" type="img/png">
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="assets/js/script.js"></script>
+</body>
+</html>
